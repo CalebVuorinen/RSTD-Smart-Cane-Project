82b19542f857b06bed4bb7d1006676cd52fb6330
Kalle Vuorinen <kalle.vuorinen@suprakraft.com>
2018-11-25 12:32:05 +0200

    test

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -8,13 +8,18 @@ import { LoginModule } from './login/login.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 @NgModule({
   imports: [
     BrowserModule,
     LoginModule,          // Eager loaded since we may need to go here right away as browser loads based on route user enters
     AppRoutingModule,     // Main routes for application
     CoreModule,           // Singleton objects (services, components that are loaded only once, etc.)
-    SharedModule          // Shared (multi-instance) objects
+    SharedModule,         // Shared (multi-instance) objects
+    HttpModule,
+    FormsModule
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
