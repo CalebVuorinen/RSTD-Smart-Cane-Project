b16631191b1717aaf4b2a41177141a7c72fe369e
Kalle Vuorinen <kalle.vuorinen@suprakraft.com>
2018-11-25 14:36:19 +0200

    Heroku mongoDB #3

--- web/server.js
+++ web/server.js
@@ -9,6 +9,8 @@ var express     = require('express'),
     customers   = JSON.parse(fs.readFileSync('data/customers.json', 'utf-8')),
     states      = JSON.parse(fs.readFileSync('data/states.json', 'utf-8'));
 
+    var mongodb = require("mongodb");
+    var ObjectID = mongodb.ObjectID;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -26,6 +28,27 @@ if (process.platform === "win32") {
     });
 }
 
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+// Connect to the database before starting the application server.
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
 database.open(() => {
     // Set NODE_ENV to 'development' and uncomment the following if to only run
     // the seeder when in dev mode
@@ -42,15 +65,9 @@ app.all('/*', function(req, res) {
     res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(3000);
+app.listen(8000);
 
-console.log('Express listening on port 3000.');
+console.log('Express listening on port 8000.');
 
-//Open browser
-var opn = require('opn');
-
-opn('http://localhost:3000').then(() => {
-    console.log('Browser closed.');
-});
 
 
