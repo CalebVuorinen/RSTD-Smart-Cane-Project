e52e9cfcbfbbecf9a1ad8a593fbe2b69882feba3
Tapio Anttila <tapio-a@hotmail.com>
2018-12-11 22:51:30 +0200

    hw final

--- hardware/SmartCane_HW.ino
+++ hardware/SmartCane_HW.ino
@@ -1,37 +1,19 @@
-/*
-
- This example connects to an unencrypted Wifi network.
- Then it prints the  MAC address of the Wifi module,
- the IP address obtained, and other network details.
-
- created 13 July 2010
- by dlf (Metodo2 srl)
- modified 31 May 2012
- by Tom Igoe
- */
 
 #include <SPI.h>
 #include <WiFiNINA.h>
-#include <ArduinoHttpClient.h>
 #include <Arduino.h>
 #include <TinyGPS++.h>
 #include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 
-char ssid[] = SECRET_SSID;        // your network SSID (name)
+char ssid[] = SECRET_SSID;    // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 TinyGPSPlus gps;
+WiFiClient client;
 
 const char WEBSITE[] = "rstd-smart-cane.herokuapp.com";
-const char path[] = "/api/customers/location?";
-int port = 8080;
-
-WiFiClient client;
-//HttpClient client = HttpClient(wifi, WEBSITE, port);
-String response;
-int statusCode = 0;
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -75,108 +57,55 @@ void loop() {
 // print your MAC address:
     byte mac[6];
     WiFi.macAddress(mac);
-    Serial.print("MAC address: ");
-    printMacAddress(mac);
-// Print GPS data
-  /*while (Serial1.available() > 0)
-    if (gps.encode(Serial1.read())){
-      // location
-      Serial.print(F("Location: ")); 
-      if (gps.location.isValid())
-      {
-        Serial.print(gps.location.lat(), 6);
-        Serial.print(F(","));
-        Serial.print(gps.location.lng(), 6);
-        }
-      else
-      {
-        Serial.print(F("INVALID"));
-      }
-      // date
-      Serial.print(F("  Date/Time: "));
-      if (gps.date.isValid())
-      {
-        Serial.print(gps.date.month());
-        Serial.print(F("/"));
-        Serial.print(gps.date.day());
-        Serial.print(F("/"));
-        Serial.print(gps.date.year());
-      }
-      else
-      {
-        Serial.print(F("INVALID"));
-      }
-      // time
-      Serial.print(F(" "));
-      if (gps.time.isValid())
-      {
-        if (gps.time.hour() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.hour());
-        Serial.print(F(":"));
-        if (gps.time.minute() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.minute());
-        Serial.print(F(":"));
-        if (gps.time.second() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.second());
-        Serial.print(F("."));
-        if (gps.time.centisecond() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.centisecond());
-      }
-      else
-      {
-        Serial.print(F("INVALID"));
-      }
-    Serial.println();
-    } */
     String m = mac2String(mac);
+    Serial.print("MAC address: ");
+    Serial.print(m);
 // form postMessage
-    /*if (client.connect(WEBSITE, 80)) {
-      Serial.println("connected");
-      String postMessage = "";
+    if (client.connect(WEBSITE, 80)) {
+      Serial.println("connected to client");
+      String postMessage = "api/customers/location?mac=";
       while (Serial1.available() > 0)
       if (gps.encode(Serial1.read())){
-      postMessage += "mac=123";
-      postMessage += "&latitudeData=" + String(gps.location.lat())
-      + "&longitudeData=" + gps.location.lng()
-      + "&dateData=" + gps.date.day() + "/" + gps.date.month() + "/" + gps.date.year()
+      postMessage += m;
+      postMessage += "&latitudeData=" + (String) gps.location.lat()
+      + "&longitudeData=" + (String) gps.location.lng()
+      + "&dateData=" + (String) gps.date.day() + "/" + (String) gps.date.month() + "/" + (String) gps.date.year()
       + "&timeData=";
       if (gps.time.hour() < 10) postMessage = postMessage + "0";
-          postMessage = postMessage + gps.time.hour();
+          postMessage = postMessage + (String) gps.time.hour();
           postMessage = postMessage + ":";
           if (gps.time.minute() < 10) postMessage = postMessage + "0";
-          postMessage = postMessage + gps.time.minute();
+          postMessage = postMessage + (String) gps.time.minute();
           postMessage = postMessage + ":";
           if (gps.time.second() < 10) postMessage = postMessage + "0";
-          postMessage = postMessage + gps.time.second();
+          postMessage = postMessage + (String) gps.time.second();
           postMessage = postMessage + ".";
           if (gps.time.centisecond() < 10) postMessage = postMessage + "0";
-          postMessage = postMessage + gps.time.centisecond();
-      }
+          postMessage = postMessage + (String) gps.time.centisecond();
 
-    }*/
+    }
 // Make a HTTP request:
-  Serial.println("making POST request");
-  String postData = "mac=123&latitudeData=65.00&longitudeData=25.47&dateData=7/12/2018&timeData=21:21:27.00";
+      Serial.println("making POST request");
+      client.print("POST "); client.print(postMessage); client.println(" HTTP/1.1");
+      client.println("Host: rstd-smart-cane.herokuapp.com");
+      client.println("User-Agent: Arduino/1.0");
+      client.println("Connection: keep-alive");
+      client.println("Content-Type: text/plain; charset=utf-8");
+      client.print("Content-Length: ");
+      client.println(postMessage.length());
+      client.println();
+      client.println(postMessage);
+      Serial.println("\nData sent");
 
-  if (client.connect(WEBSITE, 80)) {
-    Serial.println("Connected to http client");
-    client.println("POST /api/customers/location? HTTP/1.1");
-    client.println("Host:  rstd-smart-cane.herokuapp.com");
-    client.println("User-Agent: Arduino/1.0");
-    client.println("Connection: close");
-    client.println("Content-Type: application/json;");
-    client.print("Content-Length: ");
-    client.println(postData.length());
-    client.println();
-    client.println(postData);
-    Serial.println("\nData sent");
+// response
+      char c; String dataStr;
+      while(client.connected() || client.available()) {
+        c = client.read();    //read first character
+        dataStr += c;
+      }
+      Serial.println(dataStr);
+
   }
-  /*statusCode = client.responseStatusCode();
-  response = client.responseBody();
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  Serial.print("Response: ");
-  Serial.println(response);*/
   
 }
 
@@ -187,11 +116,6 @@ void printWifiData() {
   Serial.println(ip);
   Serial.println(ip);
 
-  // print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  printMacAddress(mac);
 }
 
 void printCurrentNet() {
@@ -214,19 +138,6 @@ void printCurrentNet() {
   byte encryption = WiFi.encryptionType();
   Serial.print("Encryption Type:");
   Serial.println(encryption, HEX);
-  Serial.println();
-}
-
-void printMacAddress(byte mac[]) {
-  for (int i = 5; i >= 0; i--) {
-    if (mac[i] < 16) {
-      Serial.print("0");
-    }
-    Serial.print(mac[i], HEX);
-    if (i > 0) {
-      Serial.print(":");
-    }
-  }
   Serial.println();
 }
 
